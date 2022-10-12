4a793eb1d00bc2f479042ac6e228d792eed03b6d
kiisi <kiisifelix06@gmail.com>
2022-10-12 08:56:45 +0100

    animate the logo height

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -45,11 +45,11 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: "logo",
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: 60.0,
+                    height: controller.value,
                   ),
                 ),
                 Text(
-                  '${controller.value.toInt()}%',
+                  'Flash Chat',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
