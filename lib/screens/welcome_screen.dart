57539d4a16851cb2b6ab48e4ea29879bba1968ae
kiisi <kiisifelix06@gmail.com>
2022-10-12 09:34:20 +0100

    Getting ready to pratice for color Tween

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -9,33 +9,17 @@ class WelcomeScreen extends StatefulWidget {
   _WelcomeScreenState createState() => _WelcomeScreenState();
 }
 
-class _WelcomeScreenState extends State<WelcomeScreen>
-    with SingleTickerProviderStateMixin {
-  AnimationController controller;
-  Animation animation;
-
+class _WelcomeScreenState extends State<WelcomeScreen> {
   @override
   void initState() {
     super.initState();
+  }
 
-    controller = AnimationController(
-      duration: Duration(seconds: 1),
-      vsync: this,
-    );
-    animation =
-        CurvedAnimation(parent: controller, curve: Curves.easeInOutBack);
-    controller.forward();
-    animation.addStatusListener((status) {
-      if (status == AnimationStatus.completed) {
-        controller.reverse(from: 1.0);
-      } else if (status == AnimationStatus.dismissed) {
-        controller.forward();
-      }
-    });
-    controller.addListener(() {
-      setState(() {});
-      print(animation.value);
-    });
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    /* Always dispose an animation to avoid waste of resources*/
+    super.dispose();
   }
 
   @override
@@ -54,7 +38,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   tag: "logo",
                   child: Container(
                     child: Image.asset('images/logo.png'),
-                    height: controller.value * 100,
+                    height: 60.0,
                   ),
                 ),
                 Text(
